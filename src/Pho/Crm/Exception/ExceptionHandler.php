ce83b542d46fc90cee93648c1d66cff048b9ffe5
Barun Kharel <kharelbarun@gmail.com>
2018-12-01 00:00:59 +0545

    add whoops for debugging

--- src/Pho/Crm/Exception/ExceptionHandler.php
+++ src/Pho/Crm/Exception/ExceptionHandler.php
@@ -3,6 +3,8 @@
 namespace Pho\Crm\Exception;
 
 use Teapot\StatusCode;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
 use Zend\Diactoros\Response\HtmlResponse;
 
 class ExceptionHandler
@@ -12,7 +14,19 @@ class ExceptionHandler
         switch (get_class($ex)) {
 
             default:
-                $response = new HtmlResponse(view('500.php'), StatusCode::INTERNAL_SERVER_ERROR);
+                $isDebug = config('app.debug');
+                if ($isDebug) {
+                    $whoops = new Run();
+                    $whoops->pushHandler(new PrettyPageHandler());
+                    $whoops->register();
+                    $whoops->writeToOutput(false);
+                    $whoops->allowQuit(false);
+                    $output = $whoops->handleException($ex);
+                }
+                else {
+                    $output = view('500.php');
+                }
+                $response = new HtmlResponse($output, StatusCode::INTERNAL_SERVER_ERROR);
         }
 
         return $response;
