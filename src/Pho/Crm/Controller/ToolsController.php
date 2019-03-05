6aa44c5e10f1f2794acfd0055881cc1e46378cb0
Emre <esokullu@gmail.com>
2019-03-04 22:11:55 -0800

    added tools page files

---
+++ src/Pho/Crm/Controller/ToolsController.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Pho\Crm\Controller;
+
+use Carbon\Carbon;
+use Illuminate\Database\Capsule\Manager;
+use Pho\Crm\Auth;
+use Pho\Crm\Model\ServiceConversation;
+use Pho\Crm\Model\ServiceTicket;
+use Pho\Crm\Model\User;
+use Psr\Http\Message\ServerRequestInterface;
+use Rakit\Validation\Validator;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\RedirectResponse;
+
+class ServiceTicketController
+{
+    private $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function html()
+    {
+        $user = $this->auth->getUser();
+/*
+        $tickets = ServiceTicket::query();
+
+        $tickets = $tickets->where('by', $user->id)->orWhere('assignee', $user->id)
+            ->limit(20)
+            ->offset(0)
+            ->orderBy('open_date', 'desc')
+            ->get();
+            */
+
+        return new HtmlResponse(view('tools.php', [
+            //'tickets' => $tickets,
+            //'ticketStatusToText' => $this->getTicketStatusToText(),
+            //'ticketTypeToText' => $this->getTicketTypeToText(),
+        ]));
+    }
+
+    public function conversation($uuid)
+    {
+        $ticket = ServiceTicket::where('uuid', $uuid)
+            ->with([
+                'serviceConversations' => function ($query) {
+                    $query->orderBy('created_at', 'desc');
+                },
+                'serviceConversations.user',
+                'assigneeUser',
+            ])
+            ->firstOrFail();
+        $by = User::where('id', $ticket->by)
+            ->with([
+                'instances.site',
+            ])
+            ->withCount([
+                'accessTokens' => function ($query) {
+                    $query->whereRaw('created_at > (NOW() - INTERVAL 30 DAY)');
+                },
+                'serviceConversations',
+            ])->first();
+        $conversations = $ticket->serviceConversations;
+
+        return new HtmlResponse(view('ticket_conversation.php', [
+            'ticket' => $ticket,
+            'by' => $by,
+            'conversations' => $conversations,
+            'ticketStatusToText' => $this->getTicketStatusToText(),
+            'ticketTypeToText' => $this->getTicketTypeToText(),
+            'cannedResponses' => config('crm.canned_responses'),
+        ]));
+    }
+
+    public function replyPost($uuid, ServerRequestInterface $request)
+    {
+        $ticket = ServiceTicket::where('uuid', $uuid)
+            ->with([
+                'serviceConversations' => function ($query) {
+                    $query->orderBy('created_at', 'desc');
+                },
+                'serviceConversations.user',
+                'assigneeUser',
+                'byUser',
+            ])
+            ->firstOrFail();
+        $conversations = $ticket->serviceConversations;
+
+        if ($ticket->status === ServiceTicket::STATUS_CLOSED) {
+            return new HtmlResponse(view('ticket_conversation.php', [
+                'ticket' => $ticket,
+                'conversations' => $conversations,
+                'ticketStatusToText' => $this->getTicketStatusToText(),
+                'ticketTypeToText' => $this->getTicketTypeToText(),
+                'cannedResponses' => config('crm.canned_responses'),
+                'fail_message' => 'Ticket already closed',
+            ]));
+        }
+
+        $body = $request->getParsedBody();
+
+        $validator = new Validator();
+        $validation = $validator->validate($body, [
+            'text' => 'required',
+        ]);
+        if ($validation->fails()) {
+            $errors = $validation->errors();
+            return new HtmlResponse(view('ticket_conversation.php', [
+                'ticket' => $ticket,
+                'conversations' => $conversations,
+                'ticketStatusToText' => $this->getTicketStatusToText(),
+                'ticketTypeToText' => $this->getTicketTypeToText(),
+                'cannedResponses' => config('crm.canned_responses'),
+                'body' => $body,
+                'errors' => $errors,
+            ]));
+        }
+
+        $text = $body['text'];
+        $currentUser = $this->auth->getUser();
+        $isRepliedByCreator = $ticket->byUser->id === $currentUser->id;
+        $now = Carbon::now();
+
+        Manager::connection()->beginTransaction();
+        ServiceConversation::create([
+            'uuid' => $uuid,
+            'user_id' => $this->auth->getUser()->id,
+            'text' => $text,
+            'source' => ServiceConversation::SOURCE_WEBSITE,
+            'created_at' => $now,
+        ]);
+        if ($isRepliedByCreator) {
+            $ticket->status = ServiceTicket::STATUS_OPEN;
+            $ticket->save();
+        }
+        else {
+            $ticket->status = ServiceTicket::STATUS_WAITING_RESPONSE;
+            if ($ticket->first_response_date === null) {
+                $ticket->first_response_date = $now;
+            }
+            $ticket->save();
+        }
+        Manager::connection()->commit();
+
+        return new RedirectResponse(url("service-tickets/{$uuid}"));
+    }
+
+    public function getTicketTypeToText()
+    {
+        return function ($type) {
+            $text = '';
+            switch ($type) {
+                case ServiceTicket::TYPE_SUPPORT:
+                    $text = 'Support';
+                    break;
+            }
+            return $text;
+        };
+    }
+
+    public function getTicketStatusToText()
+    {
+        return function ($status) {
+            $text = '';
+            switch ($status) {
+                case ServiceTicket::STATUS_OPEN:
+                    $text = 'Open';
+                    break;
+                case ServiceTicket::STATUS_WAITING_RESPONSE:
+                    $text = 'Waiting Response';
+                    break;
+                case ServiceTicket::STATUS_CLOSED:
+                    $text = 'Closed';
+                    break;
+            }
+            return $text;
+        };
+    }
+}
---
+++ view/tools.php
@@ -0,0 +1,13 @@
+<?php $this->layout('layout/main.php', [ 'title' => 'Service Tickets' ]) ?>
+
+<div class="container">
+    <h1>Tools</h1>
+
+    <div>
+    <ul>
+    <li>Recreate a Heroku instance only (both for Grou.ps and GraphJS) [needs email, uuid, user_id, dbid] -- for instances where we have user and instance entries in the db but no heroku</li>
+    <li>Programmatic Signup (exact same as web ui signup)</li>
+    <li>Create a new GraphJS instance from scratch (both database instance and Heroku instance) [requires a user_id only]</li>
+    </ul>
+    </div>
+</div>
